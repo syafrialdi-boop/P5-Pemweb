cdb2370ec98a319c9c68dbf766ff9241475b9d48
syafrialdi-boop <perawangsafrialdi@gmail.com>
2026-06-09 23:11:53 +0700

    php + html

--- form.php
+++ form.php
@@ -21,5 +21,23 @@
             <button>Submit</button>
         </div>
     </form>
+
+<?php # membuka tag PHP
+
+$nama = @$_GET['nama'];
+$alamat = @$_GET['alamat'];
+
+# di sini nanti kita akan tampilkan variabel $nama dan $alamat
+if ($nama) {
+    echo "<strong>Nama:</strong> {$nama} <br>";
+}
+
+if ($alamat) {
+    echo "<strong>Alamat:</strong> {$alamat} <br>";
+}
+
+# jangan lupa tutup tag PHP
+?>
+
 </body>
 </html>
