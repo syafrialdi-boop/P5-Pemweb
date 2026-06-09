308bd7bd770d69f95956beb7a7db3d9783a54a82
syafrialdi-boop <perawangsafrialdi@gmail.com>
2026-06-09 23:09:04 +0700

    html sederhana

---
+++ form.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Membuat Form Sederhana</title>
+</head>
+<body class="body-html">
+    <form>
+        <div class="nama-label">
+            <label>Nama</label> <br>
+            <input name="nama" type="text" placeholder="Masukkan nama">
+        </div>
+
+        <div class="alamat-label">
+            <label>Alamat</label> <br>
+            <input name="alamat" type="text" placeholder="Masukkan alamat">
+        </div>
+
+        <div>
+            <button>Submit</button>
+        </div>
+    </form>
+</body>
+</html>
